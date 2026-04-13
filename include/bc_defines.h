c9f2529aa93e9d637698302afceacd11d8ded345
UnRequinTigre <54859478+Chocodu31@users.noreply.github.com>
2026-04-13 11:38:01 +0200

    Update bc_defines.h
    50k pour tout utilisateur
    25k pour premier minage
    
    (Le 50k viens des documuents fourni, le 25k est une valeur arbitraire)

--- include/bc_defines.h
+++ include/bc_defines.h
@@ -16,8 +16,8 @@
 #define MAX_BLOCK 15
 #define MAXTX 10 // nb tx par bloc (tests)
 #define DIFFICULTY 4 // difficulté pour le minage
-#define INITIALREWARD 50*1000 // montant de départ de la récompence des mineurs
-#define HELIREWARD 50000 // montant de l'helicopter money
+#define INITIALREWARD 25*1000 // montant de départ de la récompence des mineurs (25k au début)
+#define HELIREWARD 50*1000 // montant de l'helicopter money (50k par utilisateur)
 #define MAX_USERS 3 // nb utilisateurs (tests)
 #define LOCK_SCRIPT_SIZE 4 // (parties 2 et 3)
 #define UNLOCK_SCRIPT_SIZE 3 // (parties 2 et 3)
